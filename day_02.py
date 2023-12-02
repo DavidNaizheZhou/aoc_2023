abd4a3fab0b7fbbe5b2bd77684805eb3de20fd29
davidnaizhezhou <david.zhou@tuwien.ac.at>
2023-12-02 21:46:47 +0100

    day_02

---
+++ day_02.py
@@ -0,0 +1,103 @@
+
+from pathlib import Path
+from enum import Enum
+import numpy as np
+import numpy.typing as npt
+import re
+
+colors = ["red", "green", "blue"]
+
+def get_n_cubes(string:str) -> int:
+    num_cubes = int(re.search("\d+", string).group())
+    return num_cubes
+
+def get_color(string:str) -> str:
+    color = re.search("|".join(colors), string).group()
+    return color
+
+def containing_colors(set_:list) -> list:
+    colors_list = []
+    for string in set_:
+        colors_list.append(get_color(string))
+    return colors_list
+
+def append_missing_entity(set_:list) -> None:
+    colors_list = containing_colors(set_) 
+    if len(set_) == 3:
+        return set_
+    
+    for color in colors:
+        if color not in colors_list:
+            set_.append(f"0 {color}")
+
+def color_sorter(string:str) -> int:
+    color = get_color(string)
+    sorting = {
+        "red":1,
+        "green":2,
+        "blue":3
+    }
+    return sorting[color]
+
+def assembly_games_array(input:str) -> np.ndarray:
+    games = []
+    for game in input.split("\n"):
+        sets_string = game.split(": ")[1].split(";")
+        sets = []
+        for set_string in sets_string:
+            set_ = set_string.split(",")
+            append_missing_entity(set_)
+            set_ = sorted(set_, key=color_sorter) # order: red, green, blue
+            sets.append([get_n_cubes(elem) for elem in set_])
+        games.append(np.array(sets).flatten())
+    max_num_elements = len(max(games, key=len))
+    games_array = np.zeros((len(games), max_num_elements))
+   
+    for i, game in enumerate(games):
+        games_array[i, :len(game)] = game
+    return games_array
+
+def get_color_columns(games_array:npt.NDArray[np.int64], index:int=0) -> npt.NDArray[np.int64]:
+    cols = np.array(range(0,games_array.shape[1], 3))+index
+    return games_array[:, cols]
+
+def count_columns(games_array:npt.NDArray[np.int64]) -> npt.NDArray[np.int64]:
+    red_cols = np.array(range(0,games_array.shape[1], 3))
+    green_cols = np.array(range(0,games_array.shape[1], 3))+1
+    blue_cols = np.array(range(0,games_array.shape[1], 3))+2
+
+    red_count = np.sum(games_array[:, red_cols], axis=1)
+    green_count = np.sum(games_array[:, green_cols], axis=1)
+    blue_count = np.sum(games_array[:, blue_cols], axis=1)
+
+    return np.stack([red_count, green_count, blue_count]).T
+
+def solution_1(input:str) -> int:
+    games_array = assembly_games_array(input)
+    reds = get_color_columns(games_array, index=0)
+    greens = get_color_columns(games_array, index=1)
+    blues = get_color_columns(games_array, index=2)
+
+    mask = (reds<=12) & (greens<=13)  & (blues<=14)
+    return np.sum(np.where(np.all(mask, axis=1))[0]+1)
+
+def solution_2(input:str) -> int:
+    games_array = assembly_games_array(input)
+    reds = get_color_columns(games_array, index=0)
+    greens = get_color_columns(games_array, index=1)
+    blues = get_color_columns(games_array, index=2)
+
+    power = np.max(reds, axis=1) * np.max(greens, axis=1) * np.max(blues, axis=1) 
+    return np.sum(power).astype(int)
+
+
+if __name__ == "__main__":
+    with open(Path("data") / "day_02" / "input.txt", mode="r", encoding="utf-8") as f:
+         input = f.read()
+        
+    sol_1 = solution_1(input)
+    print("Puzzle 1: ", sol_1)
+
+    sol_2 = solution_2(input)
+    print("Puzzle 2: ", sol_2)
+
---
+++ tests/test_day_02.py
@@ -0,0 +1,28 @@
+import day_02
+from pytest import fixture
+import numpy as np
+
+@fixture
+def example():
+    input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    return input
+
+
+def test_cube_properties(example):
+    assert day_02.get_color(" 3 blue ") == "blue"
+    assert day_02.get_n_cubes(" 4 red") == 4
+    assert day_02.get_n_cubes(" 20 red") == 20
+    assert day_02.get_n_cubes(" 200 red") == 200
+
+def test_solution_1(example):
+    assert day_02.solution_1(example) == 8
+
+def test_solution_2(example):
+    assert day_02.solution_2(example) == 2286
+
+def test_count_cubes2(example):
+    games_array = day_02.assembly_games_array(example)
+    columns = day_02.count_columns(games_array)
+
+    mask = (columns[:,0]<=12) & (columns[:,1]<=13)  & (columns[:,2]<=14)
+    assert np.sum(np.where(mask)[0]+1) == 8
