915a49073156f3edf080866cbbda727d1231712d
davidnaizhezhou <david.zhou@tuwien.ac.at>
2023-12-02 21:48:40 +0100

    refactoring day_01

--- day_01.py
+++ day_01.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 import re
 
-def day1_1_get_number(line:str) -> int:
+def get_number(line:str) -> int:
     digits = re.findall("\d", line)
     first = int(digits[0])
     last = int(digits[-1])
     return first*10 + last
 
-def day1_1_solution(input:str) -> int:
+def solution_1(input:str) -> int:
     numbers = []
     for line in input.split("\n"):
-        numbers.append(day1_1_get_number(line))
+        numbers.append(get_number(line))
     return sum(numbers)
 
 def day1_2_replace_spelled_out(input:str) -> str:
@@ -32,11 +32,11 @@ def day1_2_replace_spelled_out(input:str) -> str:
         input = input.replace(key, value)
     return input
 
-def day1_2_solution(input:str)->int:
+def solution_2(input:str)->int:
     input = day1_2_replace_spelled_out(input)
     numbers = []
     for line in input.split("\n"):
-        numbers.append(day1_1_get_number(line))
+        numbers.append(get_number(line))
     return sum(numbers)
 
 
@@ -45,5 +45,5 @@ if __name__ == "__main__":
     with open(Path("data") / "day_01" / "input.txt", mode="r", encoding="utf-8") as f:
          input = f.read()
         
-    result = day1_2_solution(input)
+    result = solution_2(input)
     print(result)
